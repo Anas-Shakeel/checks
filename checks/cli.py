d2c126640c8f485faf93c0425ca79cc4fcd42680
Anas-Shakeel <srdranas456@gmail.com>
2024-11-09 22:10:41 +0500

    Improve logic

--- checks/cli.py
+++ checks/cli.py
@@ -4,17 +4,26 @@ import sys
 from checks.cmd_parser import Parser
 from checks.commands import add, check, uncheck, delete, list_tasks
 from checks.db.json_database import load_tasks, save_tasks
+from checks.exceptions import ParseError
+
+
+PROGRAM = "checks"
+VERSION = "0.1"
 
 
 def main():
     """ The Interactive CLI Session """
 
-    print("Welcome to Checks cli!")
-    print("Type 'help' for a list of commands.")
+    print("%s version %s" % (PROGRAM.title(), VERSION))
+    print("Type 'help' for usage hint. (CTRL+C to force quit)\n")
 
     try:
         while True:
-            command = input("\nchecks> ").strip()
+            command = input("checks> ").strip()
+            # Empty?
+            if not command:
+                continue
+
             if command == "help":
                 print_help()
             else:
@@ -30,7 +39,10 @@ def main():
 def process_command(command: str):
     """ Process command using `Parser` class """
     # Parse the command
-    Parser.parse(command)
+    try:
+        Parser.parse(command)
+    except ParseError as e:
+        print("Parse error: %s" % e)
 
 
 def print_help():
