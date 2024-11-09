3f2f7650273dcebb7d6678d7cc344c1cc2ba4fe8
Anas-Shakeel <srdranas456@gmail.com>
2024-11-09 19:38:16 +0500

    Kickstart project

--- checks/cli.py
+++ checks/cli.py
@@ -1,9 +1,38 @@
 """ Core CLI loop, parsing commands and routing """
 
+import sys
+from checks.cmd_parser import Parser
+from checks.commands import add, check, uncheck, delete, list_tasks
+from checks.db.json_database import load_tasks, save_tasks
+
 
 def main():
-    print("Welcome to checks.")
+    """ The Interactive CLI Session """
+
+    print("Welcome to Checks cli!")
+    print("Type 'help' for a list of commands.")
+
+    try:
+        while True:
+            command = input("\nchecks> ").strip()
+            if command == "help":
+                print_help()
+            else:
+                process_command(command)
+    except (KeyboardInterrupt, EOFError):
+        print("Force quit.")
+        sys.exit(0)
+    except ValueError:
+        print("CLI: An unknown error occured.")
+        sys.exit(1)
 
 
-if __name__ == '__main__':
-    main()
+def process_command(command: str):
+    """ Process command using `Parser` class """
+    # Parse the command
+    Parser.parse(command)
+
+
+def print_help():
+    """ Print help text """
+    print("Help is on the way, Hang tight!")
--- checks/db/json_database.py
+++ checks/db/json_database.py
@@ -1,1 +1,14 @@
 """ JSON handling for working with json database """
+
+import json
+from pathlib import Path
+
+DB_PATH = Path("tasks.json")
+
+
+def load_tasks():
+    pass
+
+
+def save_tasks(tasks):
+    pass
