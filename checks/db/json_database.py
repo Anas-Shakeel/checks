0a059afc11bb6611f2f00b20dcdb57ceacfabe93
Anas-Shakeel <srdranas456@gmail.com>
2024-11-12 14:12:42 +0500

    Add load_tasks function

--- checks/db/json_database.py
+++ checks/db/json_database.py
@@ -7,7 +7,10 @@ DB_PATH = Path("tasks.json")
 
 
 def load_tasks():
-    pass
+    """ Read & load json file, Returns the json data as python object """
+    if DB_PATH.exists():
+        with open(DB_PATH, "r", encoding="utf-8") as file:
+            return json.load(file)
 
 
 def save_tasks(tasks):
