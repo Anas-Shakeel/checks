2f25d93fe751e26783fc27f376e1cc961ba6fff2
Anas-Shakeel <srdranas456@gmail.com>
2024-11-12 15:11:12 +0500

    Add get_current_datetime function

--- checks/utils.py
+++ checks/utils.py
@@ -1,4 +1,11 @@
 """ Contains functions that don't fit elsewhere """
 
+from datetime import datetime
+
 # Datetime format used across project
 DATE_FORMAT: str = "%d-%m-%Y %H:%M:%S"
+
+
+def get_current_datetime():
+    """ Returns the current datetime """
+    return datetime.now().strftime(DATE_FORMAT)
