8f7a77d40f1e852cac6670d74d1833913e8333fd
Anas-Shakeel <srdranas456@gmail.com>
2024-11-12 15:47:21 +0500

    Add add_task function

--- checks/db/json_database.py
+++ checks/db/json_database.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from checks.models import Task
 from checks.utils import get_current_datetime
 
+from typing import List
+
 DB_PATH = Path("tasks.json")
 
 
@@ -21,10 +23,21 @@ def load_tasks():
     return []
 
 
-def save_tasks(tasks):
+def save_tasks(tasks: List[Task]):
     """ Save/Write `tasks` to json file/database """
     if not tasks:
         return
 
     with open(DB_PATH, "w", encoding="utf-8") as file:
         json.dump([task.to_dict() for task in tasks], file, indent=2)
+
+
+def add_task(description: str) -> Task:
+    """ Add a new task `description` to the database. Returns the new task """
+    tasks = load_tasks()
+
+    new_task = Task(description=description)
+    tasks.append(new_task)
+    save_tasks(tasks)
+
+    return new_task
