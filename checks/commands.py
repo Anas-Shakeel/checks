48fda81d31b326f8651638cffc669ebc4fbd407d
anas-shakeel <srdranas456@gmail.com>
2024-11-14 02:58:26 +0500

    Add add_tasks function

--- checks/commands.py
+++ checks/commands.py
@@ -2,6 +2,22 @@ from checks.db.json_database import db
 from tabulate import tabulate
 
 
+def add_tasks(descriptions):
+    """ Add tasks into the database. Multiple tasks supported. """
+    if not descriptions:
+        print("No tasks were added.")
+        return
+
+    if isinstance(descriptions, str):
+        # Add single task
+        db.add_task(descriptions)
+        print("Task added: '%s'" % descriptions)
+    else:
+        # Add bulk tasks
+        db.add_tasks(descriptions)
+        print("Tasks added.")
+
+
 def list_tasks():
     """ Print all tasks all tasks in a tabular format """
     tasks = [task.to_dict() for task in db.list_tasks()]
